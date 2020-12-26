edf641f7b5677069b94f4a28a1f033371fd3e146
jeneve11 <49526312+jeneve11@users.noreply.github.com>
2020-12-26 21:05:26 +0900

    version 1.1
    make button size bigger / new function : delete one from the list

--- farmCopy.py
+++ farmCopy.py
@@ -2,16 +2,31 @@ import tkinter, clipboard
 
 window = tkinter.Tk()
 window.title("farm_macro")
-window.geometry("660x450")
+window.geometry("580x311")
 
 def letsGo():
     global index
     clipboard.copy(farmList[index])
     index += 1
+    print(index)
+    print(farmList)
+    
+def letsDelete():
+    global farmList
+    if index < 1:
+        return
+    else:
+        farmList.pop(index-1)
+        print(farmList)
+        f = open('farmlist_신수.txt', 'w', encoding = 'utf-8')
+        for farm in farmList:
+           f.writelines(farm + '\n')
+        f.close()
+        
+btn_1 = tkinter.Button(window, height=20, width=40, text='Go', command = letsGo).grid(row=0, column=1)
+btn_2 = tkinter.Button(window, height=20, width=40, text='Delete', command = letsDelete).grid(row=0, column=2)
 
-btn = tkinter.Button(window, height=100, width=100, text='', command = letsGo).grid(row=0, column=1)
-
-f = open('farmlist_신수.txt', encoding = 'utf-8')
+f = open('farmlist_신수.txt', 'r', encoding = 'utf-8')
 line = f.readline()
 index = 0
 farmList = []
@@ -25,7 +40,10 @@ while line:
         farmName = lastWord
     farmList.append(farmName)
     line = f.readline()
-    
 f.close()
 print(farmList)
-  
+
+f = open('farmlist_신수.txt', 'w', encoding = 'utf-8')
+for farm in farmList:
+    f.writelines(farm + '\n')
+f.close()
