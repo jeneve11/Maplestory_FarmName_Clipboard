dc33833e5b65d224d4d7b5e81ad6682255d0724f
jeneve11 <49526312+jeneve11@users.noreply.github.com>
2021-01-19 22:37:54 +0900

    Add files via upload

--- farmCopy.py
+++ farmCopy.py
@@ -2,31 +2,41 @@ import tkinter, clipboard
 
 window = tkinter.Tk()
 window.title("farm_macro")
-window.geometry("580x311")
+window.geometry("580x333")
+
+entry = tkinter.Entry(window)
 
 def letsGo():
     global index
     clipboard.copy(farmList[index])
     index += 1
-    print(index)
+    print(farmList[index-1])
     print(farmList)
+    entry.delete(0, tkinter.END)
+    entry.insert(0, farmList[index-1])
     
 def letsDelete():
-    global farmList
+    global farmList, index
     if index < 1:
         return
     else:
         farmList.pop(index-1)
+        clipboard.copy(index-1)
+        print(farmList[index-1])
         print(farmList)
-        f = open('farmlist_신수.txt', 'w', encoding = 'utf-8')
+        entry.delete(0, tkinter.END)
+        entry.insert(0, farmList[index-1])
+        f = open(file_name, 'w', encoding = 'utf-8')
         for farm in farmList:
            f.writelines(farm + '\n')
         f.close()
         
-btn_1 = tkinter.Button(window, height=20, width=40, text='Go', command = letsGo).grid(row=0, column=1)
-btn_2 = tkinter.Button(window, height=20, width=40, text='Delete', command = letsDelete).grid(row=0, column=2)
+btn_1 = tkinter.Button(window, height=20, width=40, text='다음 농장', command = letsGo).grid(row=0, column=1)
+btn_2 = tkinter.Button(window, height=20, width=40, text='다음 농장(삭제)', command = letsDelete).grid(row=0, column=2)
+entry.grid(row=1, column=0, columnspan=2)
 
-f = open('farmlist_신수.txt', 'r', encoding = 'utf-8')
+file_name = input("Input your text file name: ") + '.txt'
+f = open(file_name, 'r', encoding = 'utf-8')
 line = f.readline()
 index = 0
 farmList = []
@@ -43,7 +53,7 @@ while line:
 f.close()
 print(farmList)
 
-f = open('farmlist_신수.txt', 'w', encoding = 'utf-8')
+f = open(file_name, 'w', encoding = 'utf-8')
 for farm in farmList:
     f.writelines(farm + '\n')
 f.close()
