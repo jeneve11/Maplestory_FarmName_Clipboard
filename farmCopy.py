29173b7ff34969c6af566a411aec5e371e3e5c9c
jeneve11 <49526312+jeneve11@users.noreply.github.com>
2021-02-05 08:59:10 +0900

    version 1.3
    delete 버튼 오작동 수정

--- farmCopy.py
+++ farmCopy.py
@@ -1,10 +1,13 @@
 import tkinter, clipboard
-
+# 중복제거 / input 콘솔말고 팝업으로 / 
 window = tkinter.Tk()
 window.title("farm_macro")
 window.geometry("580x333")
 
-entry = tkinter.Entry(window)
+entry_1 = tkinter.Entry(window)
+entry_1.grid(row=1, column=0, columnspan=2)
+entry_2 = tkinter.Entry(window)
+entry_2.grid(row=1, column=2, columnspan=2)
 
 def letsGo():
     global index
@@ -12,8 +15,10 @@ def letsGo():
     index += 1
     print(farmList[index-1])
     print(farmList)
-    entry.delete(0, tkinter.END)
-    entry.insert(0, farmList[index-1])
+    entry_1.delete(0, tkinter.END)
+    entry_1.insert(0, farmList[index-1])
+    entry_2.delete(0, tkinter.END)
+    entry_2.insert(0, str(index)+'/'+str(len(farmList)))
     
 def letsDelete():
     global farmList, index
@@ -21,20 +26,21 @@ def letsDelete():
         return
     else:
         farmList.pop(index-1)
-        clipboard.copy(index-1)
+        clipboard.copy(farmList[index-1])
         print(farmList[index-1])
         print(farmList)
-        entry.delete(0, tkinter.END)
-        entry.insert(0, farmList[index-1])
+        entry_1.delete(0, tkinter.END)
+        entry_1.insert(0, farmList[index-1])
+        entry_2.delete(0, tkinter.END)
+        entry_2.insert(0, str(index)+'/'+str(len(farmList)))
         f = open(file_name, 'w', encoding = 'utf-8')
         for farm in farmList:
            f.writelines(farm + '\n')
         f.close()
-        
+
 btn_1 = tkinter.Button(window, height=20, width=40, text='Next Farm', command = letsGo).grid(row=0, column=1)
 btn_2 = tkinter.Button(window, height=20, width=40, text='Next Farm(Delete)', command = letsDelete).grid(row=0, column=2)
-entry.grid(row=1, column=0, columnspan=2)
-
+        
 file_name = input("Input your text file name: ") + '.txt'
 f = open(file_name, 'r', encoding = 'utf-8')
 line = f.readline()
